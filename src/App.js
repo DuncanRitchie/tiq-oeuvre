9fad58325c073f0d14a5dcc3937b25e6cea31d6c
DuncanRitchie <duncanritchie@btinternet.com>
2019-04-10 10:55:55 +0100

    Imported data.json into state.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import data from "./data.json";
 import './App.css';
 
 class App extends Component {
+  state = {
+    allData: []
+  }
+  componentDidMount() {
+    this.setState({allData: data})
+  }
   render() {
     return (
       <div className="App">
