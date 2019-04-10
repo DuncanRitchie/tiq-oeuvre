54753cf64a7f6d98335bfc8006aa7699ea945eff
Duncan Ritchie <duncanritchie@btinternet.com>
2019-04-10 23:17:52 +0100

    Got the Json and props working nicely.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import data from "./data.json";
+import PlayDetails from "./Play/PlayDetails";
 import './App.css';
 
 class App extends Component {
   state = {
-    allData: []
+    allData: data
   }
-  componentDidMount() {
-    this.setState({allData: data})
-  }
+  
   render() {
+    let allPlays = this.state.allData.reverse().map((play, index) => {
+      return (
+        <PlayDetails key={index} title={play.title}
+        epoch={play.epoch}
+        datePrecision={play["date-precision"]}
+        datesAsText={play["dates-as-text"]}
+        verb={play.verb}
+        by={play.by}
+        myActingRole={play["my-acting-role"]}
+        tags={play["tags-batch-one"].concat(play["tags-batch-two"])}
+        posterOrientation={play["poster-orientation"]}
+        image500={play["photo-url-max-width-500"]}/>
+      )
+    })
     return (
       <div className="App">
-        Hello, world!
+        {allPlays}
       </div>
     );
   }
--- src/Play/PlayDetails.jsx
+++ src/Play/PlayDetails.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import "./PlayDetails.css"
 
-let PlayDetails = (props) => {
+const PlayDetails = (props) => {
     return (
         <div class="play-details">
-            <img src={props.photo-url-max-width-500} alt={props.title}/>
+            {props.image500 ? <img src={props.image500} alt={ReactHtmlParser(props.title)} class={props.posterOrientation}/> : null}
+            <div class={props.image500 ? "play-text" : "play-text-only"}>
+                <h1 class="play-title">{ReactHtmlParser(props.title)}</h1>
+                <p>{props.verb} <strong>{ReactHtmlParser(props.datesAsText)}</strong> {props.datePrecision == "month" ? <span>(I can&rsquo;t be more precise than that) </span> : null } by {ReactHtmlParser(props.by)}</p>
+                <p>My roles: 
+                {props.tags.includes("Duncan as Actor") ? <span className="tag"><strong>actor</strong></span> : null}
+                {props.tags.includes("Duncan as Poster Designer") ? <span className="tag"><strong>poster</strong> designer</span> : null}
+                {props.tags.includes("Duncan as Programme Designer") ? <span className="tag"><strong>programme</strong> designer</span> : null}
+                {props.tags.includes("Duncan as Lyricist") ? <span className="tag"><strong>lyricist</strong></span> : null}
+                {props.tags.includes("Duncan as Photographer") ? <span className="tag"><strong>photographer</strong></span> : null}
+                {props.tags.includes("Duncan as Assistant Director") ? <span className="tag"><strong>assistant-director</strong></span> : null}
+                {props.tags.includes("Duncan as CoDesigner") ? <span className="tag"><strong>poster</strong> co-designer (with illustration by Alison Pitt)</span> : null}
+                </p>
+            </div>
         </div>
     )
 }
+
+export default PlayDetails
