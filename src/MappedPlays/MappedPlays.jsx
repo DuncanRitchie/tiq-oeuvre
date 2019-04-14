ebb673de3e083c7e4675336e9c52db6eb05b7aec
Duncan Ritchie <duncanritchie@btinternet.com>
2019-04-14 22:24:19 +0100

    Added footer

--- src/MappedPlays/MappedPlays.jsx
+++ src/MappedPlays/MappedPlays.jsx
@@ -5,6 +5,11 @@ const MappedPlays = (props) => {
     return (
         <div className="mapped-plays">
             {props.mappedPlays}
+            <div className="footer">
+                <p>
+                    Made by Duncan Ritchie. <a href="https://www.duncanritchie.co.uk" title="Duncan Ritchie&rsquo;s website">My website</a>. <a href="theatreinthequarter.co.uk" title="Theatre in the Quarter&rsquo;s website">TiQ&rsquo;s website</a>.
+                </p>
+            </div>
         </div>
     )
 }
