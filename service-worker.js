9c23faf192e7e4be826a29720022be3b08ca761a
Administrator <administrator@CN-7.lan>
2019-04-24 15:52:19 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tiq-oeuvre/precache-manifest.9f3ee3b8993ccd29b6a58a63c433f730.js"
+  "/tiq-oeuvre/precache-manifest.e6902bc0e056b9d45a672ba68fa035ee.js"
 );
 
 workbox.clientsClaim();
