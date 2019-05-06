552314d9dc378aa8c10b6930bef303c313160676
DuncanRitchie <duncanritchie@btinternet.com>
2019-05-06 12:46:09 +0100

    "Theatre in the Quarter and related groups"

--- src/HeaderBar/HeaderBar.jsx
+++ src/HeaderBar/HeaderBar.jsx
@@ -3,7 +3,7 @@ import './HeaderBar.css';
 const HeaderBar = (props) => {
     return (
         <div className="header-bar">
-            <h1 className="main-header">My Theatre in the Quarter &oelig;uvre</h1>
+            <h1 className="main-header">My &oelig;uvre with Theatre in the Quarter and related groups</h1>
             <p className="filter-paragraph">{props.filterParagraph}. {props.rubric}&ensp;
             {props.filter.slug === "" && 
             props.filter.troupe === "" && 
