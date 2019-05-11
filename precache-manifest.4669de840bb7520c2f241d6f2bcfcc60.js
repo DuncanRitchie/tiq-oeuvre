b4296ab11c5edb9cab46b6e120bc155d8d04b342
DuncanRitchie <duncanritchie@btinternet.com>
2019-05-11 19:45:27 +0100

    Updates

--- precache-manifest.4669de840bb7520c2f241d6f2bcfcc60.js
+++ precache-manifest.4669de840bb7520c2f241d6f2bcfcc60.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/tiq-oeuvre/static/js/runtime~main.393755fd.js"
   },
   {
-    "revision": "7b761b6391c0391cafc9",
-    "url": "/tiq-oeuvre/static/js/main.98dd7323.chunk.js"
+    "revision": "449e42f7a315313d9247",
+    "url": "/tiq-oeuvre/static/js/main.cc83cb42.chunk.js"
   },
   {
     "revision": "c0096aaae1e17732a02e",
     "url": "/tiq-oeuvre/static/js/2.0fe1f0ae.chunk.js"
   },
   {
-    "revision": "7b761b6391c0391cafc9",
+    "revision": "449e42f7a315313d9247",
     "url": "/tiq-oeuvre/static/css/main.c813894e.chunk.css"
   },
   {
-    "revision": "0c6d24d329e78b95f720e0807882221b",
+    "revision": "0eb28ab3bbacfb4ac0e3a72d9d3dd435",
     "url": "/tiq-oeuvre/index.html"
   }
 ];
