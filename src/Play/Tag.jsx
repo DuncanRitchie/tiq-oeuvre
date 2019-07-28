02f3bbba82496cc939c4aed5f40670728ec2e64a
Duncan Ritchie <duncanritchie@btinternet.com>
2019-07-28 16:53:30 +0100

    Tag now has a title attribute

--- src/Play/Tag.jsx
+++ src/Play/Tag.jsx
@@ -3,7 +3,18 @@ import ReactHtmlParser from 'react-html-parser';
 import "./Tag.css"
 
 let Tag = (props) => {
-    return <span className="tag" onClick={props.handler}>{ReactHtmlParser(`<span class="first-word">${props.text.split(" ")[0]}</span>${props.text.replace(props.text.split(" ")[0],"")}`)}</span>
+    // The first word of the text is displayed in bold, the rest in normal weight.
+    const firstWord = props.text.split(" ")[0]
+    const restOfText = props.text.replace(firstWord,"")
+    return (
+        <span 
+        className="tag" 
+        title={"See all results for "+ReactHtmlParser(firstWord)} 
+        onClick={props.handler}
+        >
+            {ReactHtmlParser(`<span class="first-word">${firstWord}</span>${restOfText}`)}
+        </span>
+    )
 }
 
 export default Tag
