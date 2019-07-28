e121792cf0283f66f1c66b6223ce55c0510610b0
Duncan Ritchie <duncanritchie@btinternet.com>
2019-07-28 19:41:17 +0100

    Updates

---
+++ precache-manifest.0f03ed714fd6404ce796c4c5260615c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167fa30b6a059bbecebf2b08bcc65b94",
+    "url": "/tiq-oeuvre/index.html"
+  },
+  {
+    "revision": "58753d823fe64c75e714",
+    "url": "/tiq-oeuvre/static/css/main.78e5acb5.chunk.css"
+  },
+  {
+    "revision": "53503ac0afa32e6eaded",
+    "url": "/tiq-oeuvre/static/js/2.0615b329.chunk.js"
+  },
+  {
+    "revision": "58753d823fe64c75e714",
+    "url": "/tiq-oeuvre/static/js/main.647b5b88.chunk.js"
+  },
+  {
+    "revision": "c048d1742536fc81dbb9",
+    "url": "/tiq-oeuvre/static/js/runtime~main.393755fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tiq-oeuvre/precache-manifest.3dc84ab287b7f0a7931ff01bc111186b.js"
+  "/tiq-oeuvre/precache-manifest.0f03ed714fd6404ce796c4c5260615c4.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tiq-oeuvre/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tiq-oeuvre/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
