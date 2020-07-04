a5c71bd90b778f324c8bf8b8ac1670b9c0ab0b8b
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2020-07-04 22:42:52 +0100

    Added <picture> in Image, commented out due to CSS

--- src/Play/Image.jsx
+++ src/Play/Image.jsx
@@ -5,11 +5,16 @@ import convertCloudinaryUrl from "./convert-cloudinary-url";
 let Image = (props) => {
 
     return (
-        <img
-            src={convertCloudinaryUrl(props.cloudinary,400,null)}
-            alt={ReactHtmlParser(props.title)+" "+props.imageType}
-            className={props.posterOrientation}
-        />
+        // I’ll comment out the <picture> code if/when I want to deal with the CSS issues it creates.
+        // (Ie, the image appears too small at certain viewport sizes  if it’s inside the <picture> element.)
+        //<picture>
+            //<source media="(min-width:0px)" srcset={convertCloudinaryUrl(props.cloudinary,400,"webp")}/>
+            <img
+                src={convertCloudinaryUrl(props.cloudinary,400,null)}
+                alt={ReactHtmlParser(props.title)+" "+props.imageType}
+                className={props.posterOrientation}
+            />
+        //</picture>
     )
 }
 
